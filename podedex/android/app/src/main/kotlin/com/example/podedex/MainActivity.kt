c5ec861b0b84a2e15c3bb9a77fbcd7c4044d84f0
Sita Bérété <sita.berete.3@gmail.com>
2022-02-23 17:55:10 +0000

    Initialize Flutter project
      - Generate Project with vs-code
      - Remove sample code and comments
      - Rename README.md to DOC.md according to the Assignment Instructions

---
+++ podedex/android/app/src/main/kotlin/com/example/podedex/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.podedex
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ podedex/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pokedex',
+      theme: ThemeData(
+        // TODO set appropriated values
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(),
+    );
+  }
+}
